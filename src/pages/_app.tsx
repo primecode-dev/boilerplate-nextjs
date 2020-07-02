3836f7e6529945d67912562431c31817c55b7cf1
Francisross Soares de Oliveira <franciscpd@gmail.com>
2020-07-02 17:01:30 -0300

    feat: add favicon and manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,11 +8,27 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <title>Boilerplate Next.JS</title>
-        <link rel="shortcut icon" href="/assets/images/favicon/icon-192.png" />
         <link
           rel="apple-touch-icon"
-          href="/assets/images/favicon/apple-touch-icon.png"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3423A6" />
+        <meta name="msapplication-TileColor" content="#3423a6" />
+        <meta name="theme-color" content="#ffffff" />
         <meta
           name="description"
           content="A simple project starter to work with typescript, react, next.js and styled component"
