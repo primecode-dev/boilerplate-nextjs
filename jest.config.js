1fb78b7b184ebe1fc982f17ae82ab9f12386b1a7
Francisross Soares de Oliveira <franciscpd@gmail.com>
2020-07-02 17:00:32 -0300

    fix: test with ts and tsx files

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.(ts|tsx)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
